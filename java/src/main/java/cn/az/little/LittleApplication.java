2866b9ae31328ed6e1e40a5a54eee623edeaa434
AzusaChino <azusa146@gmail.com>
2019-10-19 20:51:24 +0800

    commit two_sum_001

---
+++ java/src/main/java/cn/az/little/LittleApplication.java
@@ -0,0 +1,13 @@
+package cn.az.little;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleApplication.class, args);
+    }
+
+}

5cf6a63a3976e840dcf8fffffac1c076bc42b7a7
AzusaChino <azusa146@gmail.com>
2019-11-06 19:38:16 +0800

    same tree 100 easy

---
+++ java/src/main/java/cn/az/little/common/TreeNode.java
@@ -0,0 +1,12 @@
+package cn.az.little.common;
+
+/**
+ * @author : Liz
+ * @date : 2019/11/6
+ **/
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+}

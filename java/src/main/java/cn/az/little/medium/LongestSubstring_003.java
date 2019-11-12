116a367f1d49263110ff82550f1877462fb6222e
AzusaChino <azusa146@gmail.com>
2019-11-12 18:46:28 +0800

    longest substr not repeat 003 medium

---
+++ java/src/main/java/cn/az/little/medium/LongestSubstring_003.java
@@ -0,0 +1,32 @@
+package cn.az.little.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : Liz
+ * @date : 2019/11/12
+ **/
+public class LongestSubstring_003 {
+    public static void main(String[] args) {
+        System.out.println(solution("pepper"));
+    }
+
+    public static int solution(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>(16);
+        int len = s.length();
+        int result = 0;
+        for (int i = 0, j = 0; i < len; i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            result = Math.max(result, i - j + 1);
+
+        }
+        return result;
+    }
+}

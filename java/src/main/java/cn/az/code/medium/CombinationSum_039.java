9a7d3c342d1c145d0fc4f52d8859d1f68aba5a91
AzusaChino <azusa146@gmail.com>
2019-12-10 13:11:17 +0800

    39. Combination Sum

---
+++ java/src/main/java/cn/az/code/medium/CombinationSum_039.java
@@ -0,0 +1,38 @@
+package cn.az.code.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author azusachino
+ * @version 12/10/2019
+ */
+public class CombinationSum_039 {
+
+    public static void main(String[] args) {
+        CombinationSum_039 combinationSum039 = new CombinationSum_039();
+        System.out.println(combinationSum039.combinationSum(new int[]{1, 2, 3, 4}, 2));
+    }
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain < 0) {
+        } else if (remain == 0) {
+            list.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                tempList.add(nums[i]);
+                // not i + 1 because we can reuse same elements
+                backtrack(list, tempList, nums, remain - nums[i], i);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
--- java/src/main/java/cn/az/code/medium/ValidSudoku_036.java
+++ java/src/main/java/cn/az/code/medium/ValidSudoku_036.java
@@ -34,4 +34,26 @@ public class ValidSudoku_036 {
         }
         return true;
     }
+
+    public boolean isValidSudoku2(char[][] board) {
+        for(int i = 0;i < 9;i++){
+            boolean[] rowCheck = new boolean[9];
+            boolean[] colCheck = new boolean[9];
+            boolean[] boxCheck = new boolean[9];
+            for(int j = 0;j< 9;j++){
+                if(board[i][j] == '.'){}
+                else if(rowCheck[board[i][j] - '1']) return false;
+                else rowCheck[board[i][j] - '1'] = true;
+                if(board[j][i] == '.'){}
+                else if(colCheck[board[j][i] - '1']) return false;
+                else colCheck[board[j][i] - '1'] = true;
+                int m = i/3*3 + j/3;
+                int n = i%3*3 + j%3;
+                if(board[m][n] == '.') {}
+                else if(boxCheck[board[m][n] - '1']) return false;
+                else boxCheck[board[m][n] - '1'] = true;
+            }
+        }
+        return true;
+    }
 }

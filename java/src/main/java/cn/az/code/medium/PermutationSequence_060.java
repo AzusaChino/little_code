8d56988d3911e7bd00ade21896133b738f303ad4
AzusaChino <azusa146@gmail.com>
2020-01-06 09:01:58 +0800

    60. Permutation Sequence medium

---
+++ java/src/main/java/cn/az/code/medium/PermutationSequence_060.java
@@ -0,0 +1,33 @@
+package cn.az.code.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Liz
+ * @date 1/6/2020
+ */
+public class PermutationSequence_060 {
+    public static void main(String[] args) {
+        PermutationSequence_060 permutationSequence060 = new PermutationSequence_060();
+        System.out.println(permutationSequence060.getPermutation(4, 12));
+    }
+
+    public String getPermutation(int n, int k) {
+
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            list.add(i);
+        }
+        for (int i = 0, l = k - 1; i < n; i++) {
+            fact /= (n - i);
+            int index = (l / fact);
+            sb.append(list.remove(index));
+            l -= index * fact;
+        }
+        return sb.toString();
+    }
+}

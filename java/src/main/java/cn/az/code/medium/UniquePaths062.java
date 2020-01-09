475df3826b14a6bae7c84705606d615636c8d6dd
AzusaChino <azusa146@gmail.com>
2020-01-09 09:07:26 +0800

    63. Unique Paths II medium (using DP)

--- java/src/main/java/cn/az/code/medium/UniquePaths062.java
+++ java/src/main/java/cn/az/code/medium/UniquePaths062.java
@@ -1,14 +1,19 @@
 package cn.az.code.medium;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
 /**
  * @author Liz
  * @date 1/8/2020
  */
+@Slf4j
 public class UniquePaths062 {
 
     public static void main(String[] args) {
         UniquePaths062 uniquePaths062 = new UniquePaths062();
-        System.out.println(uniquePaths062.uniquePath(7, 3));
+        System.out.println(uniquePaths062.uniquePath(3, 3));
     }
 
     public int uniquePath(int m, int n) {
@@ -22,6 +27,7 @@ public class UniquePaths062 {
                 }
             }
         }
+        log.info(Arrays.deepToString(dp));
         return dp[m - 1][n - 1];
     }
 }

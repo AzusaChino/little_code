96eb189e826976c2d3b1bdc8111083db06757bad
AzusaChino <azusa146@gmail.com>
2020-01-20 09:10:04 +0800

    82. Remove Duplicates from sorted List II (LinkedList)

--- java/src/main/java/cn/az/code/common/ListNode.java
+++ java/src/main/java/cn/az/code/common/ListNode.java
@@ -15,6 +15,19 @@ public class ListNode {
         val = x;
     }
 
+
+    public static ListNode forSimpleTest(int... nums) {
+        ListNode head = new ListNode(0);
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        for (int n : nums) {
+            head.next = new ListNode(n);
+            head = head.next;
+        }
+        return dummy.next;
+    }
+
+
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();

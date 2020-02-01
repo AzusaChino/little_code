acd7cca94929905ae1290628f69706796067e39d
AzusaChino <azusa146@gmail.com>
2020-02-01 21:12:16 +0800

    :dog: ft.99.Recover Binary Search Tree

--- python/common/__init__.py
+++ python/common/__init__.py
@@ -2,3 +2,13 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)

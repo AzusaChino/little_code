b46e95ebc900be0c050ebf5a27d8835f9cc8cceb
AzusaChino <azusa146@gmail.com>
2020-04-08 17:25:10 +0800

    ft. add divide conquer func model

--- python/common/__init__.py
+++ python/common/__init__.py
@@ -12,3 +12,22 @@ class TreeNode:
 
     def __repr__(self):
         return 'TreeNode({})'.format(self.val)
+
+def divide_conquer(problem, param1, param2...):
+    # recursion terminator
+    if problem is None:
+        print_resule
+        return
+    # prepare data
+    data = prepare_data(problem)
+    sub_problems = split_problem(problem, data)
+
+    # conquer sub problems
+    sub_result1 = self.divide_conquer(sub_problems[0], p1, ...)
+    sub_result2 = self.divide_conquer(sub_problems[1], p1, ...)
+    sub_result3 = self.divide_conquer(sub_problems[2], p1, ...)
+
+    # process and generate the final result
+    result = process_result(sub_result1, sub_result2, ...)
+
+    # revert the current level states

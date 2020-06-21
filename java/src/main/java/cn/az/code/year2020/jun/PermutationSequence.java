d45ef1826dcedd7015d1782b6f62c58a73da743d
AzusaChino <azusa146@gmail.com>
2020-06-21 23:56:59 +0800

    ft. 06/21

---
+++ java/src/main/java/cn/az/code/year2020/jun/PermutationSequence.java
@@ -0,0 +1,43 @@
+package cn.az.code.year2020.jun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author az
+ * @since 2020-06-21 22:51
+ */
+public class PermutationSequence {
+
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n + 1];
+        StringBuilder sb = new StringBuilder();
+
+        // create an array of factorial lookup
+        int sum = 1;
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+            factorial[i] = sum;
+        }
+        // factorial[] = {1, 1, 2, 6, 24, ... n!}
+
+        // create a list of numbers to get indices
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+        }
+        // numbers = {1, 2, 3, 4}
+
+        k--;
+
+        for (int i = 1; i <= n; i++) {
+            int index = k / factorial[n - i];
+            sb.append(numbers.get(index));
+            numbers.remove(index);
+            k -= index * factorial[n - i];
+        }
+
+        return String.valueOf(sb);
+    }
+}

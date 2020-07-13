75fec39e731215bf75acdf0a519a989b54c83a0c
AzusaChino <azusa146@gmail.com>
2020-07-14 00:07:24 +0800

    ft. 07/13 amend

---
+++ go/src/2020/july/same-tree.go
@@ -0,0 +1,13 @@
+package july
+
+import . "common"
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil || q == nil {
+		return p == q
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}

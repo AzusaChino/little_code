19ac5e6ef5d7d386a60c50fc92d096fe698cd164
AzusaChino <azusa146@gmail.com>
2020-07-19 20:22:34 +0800

    ft. 07/19 add binary

---
+++ python/2020/july/add-binary.py
@@ -0,0 +1,11 @@
+class Solution:
+    # recursive solution
+    def add_binary(self, a, b):
+        if len(a) == 0: return b
+        if len(b) == 0: return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.add_binary(self.add_binary(a[0:-1], b[0:-1]), '1') + '0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.add_binary(a[0:-1], b[0:-1]) + '0'
+        else:
+            return self.add_binary(a[0:-1], b[0:-1]) + '1'

34a52554525aafbb6884e9b185795fb2bfa51bac
AzusaChino <azusa146@gmail.com>
2020-07-22 19:56:23 +0800

    ft. 07/22 zigzag binary tree

---
+++ go/src/2020/july/binary-tree-zigzag-order-traversal.go
@@ -0,0 +1,33 @@
+package july
+
+import (
+	. "common"
+)
+
+func bfs(s *[][]int, level int, root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	if len(*s) == level {
+		*s = append(*s, []int{})
+	}
+
+	if level%2 == 1 {
+		(*s)[level] = append([]int{root.Val}, (*s)[level]...)
+	} else {
+		(*s)[level] = append((*s)[level], root.Val)
+	}
+	for _, v := range []*TreeNode{root.Left, root.Right} {
+		bfs(s, level+1, v)
+	}
+}
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	var s [][]int
+	bfs(&s, 0, root)
+	return s
+}

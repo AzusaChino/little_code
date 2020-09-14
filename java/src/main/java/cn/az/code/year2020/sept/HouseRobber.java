cbb21e763e1344dd9aef8dc33ce30ced162cae62
AzusaChino <azusa146@gmail.com>
2020-09-14 22:56:26 +0800

    ft. 09/14 house robber

---
+++ java/src/main/java/cn/az/code/year2020/sept/HouseRobber.java
@@ -0,0 +1,36 @@
+package cn.az.code.year2020.sept;
+
+/**
+ * @author az
+ * @since 09/14/20
+ */
+public class HouseRobber {
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = dp[i - 1][0] + nums[i];
+        }
+        return Math.max(dp[n - 1][0], dp[n - 1][1]);
+    }
+
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int prev1 = 0, prev2 = 0;
+        for (int num : nums) {
+            int temp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = temp;
+        }
+        return prev1;
+    }
+}

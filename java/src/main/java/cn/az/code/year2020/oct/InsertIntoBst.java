78f22f5b2278b87778e625b4957697f6a5d83dae
AzusaChino <azusa146@gmail.com>
2020-10-08 13:38:12 +0800

    ft. append

--- java/src/main/java/cn/az/code/year2020/oct/InsertIntoBst.java
+++ java/src/main/java/cn/az/code/year2020/oct/InsertIntoBst.java
@@ -1,7 +1,46 @@
-package cn.az.code.year2020.oct;/**
+package cn.az.code.year2020.oct;
+
+import cn.az.code.common.TreeNode;
+
+/**
  * little-code
  *
  * @author azusachino
  * @since 6/10/2020 21:26
- */public class InsertIntoBst {
+ */
+public class InsertIntoBst {
+
+    public TreeNode insert(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.right = insert(root.right, val);
+        }
+        if (root.val < val) {
+            root.left = insert(root.left, val);
+        }
+        return root;
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val) {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+            } else {
+                root.right = insertIntoBST(root.right, val);
+            }
+        }
+        if (root.val > val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+            } else {
+                root.left = insertIntoBST(root.left, val);
+            }
+        }
+        return root;
+    }
 }

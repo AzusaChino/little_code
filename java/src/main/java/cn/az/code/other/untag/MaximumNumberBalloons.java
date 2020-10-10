08ad34f0c2d56222cfbe31c0d3604419497a52da
AzusaChino <azusa146@gmail.com>
2020-10-10 21:42:16 +0800

    ft. 10/10 maximum number arrow balloons

---
+++ java/src/main/java/cn/az/code/other/untag/MaximumNumberBalloons.java
@@ -0,0 +1,21 @@
+package cn.az.code.other.untag;
+
+/**
+ * @author az
+ * @since 10/10/20
+ */
+public class MaximumNumberBalloons {
+
+    public int maxNumberOfBalloons(String text) {
+        int[] chars = new int[26]; //count all letters
+        for (char c : text.toCharArray()) {
+            chars[c - 'a']++;
+        }
+        int min = chars[1];//for b
+        min = Math.min(min, chars[0]);//for a
+        min = Math.min(min, chars[11] / 2);// for l /2
+        min = Math.min(min, chars[14] / 2);//similarly for o/2
+        min = Math.min(min, chars[13]);//for n
+        return min;
+    }
+}

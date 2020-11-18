b13da326fc978641bbaa2e386b4cf6bdfd43d8b0
AzusaChino <azusa146@gmail.com>
2020-11-18 21:13:55 +0800

    11/18 merge intervals

---
+++ python/2020/nov/merge-intervals.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return intervals
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for i in intervals:
+            if not merged or merged[-1][1] < i[0]:
+                merged.append(i)
+            else:
+                merged[-1][1] = max(merged[-1][1], i[1])
+        return merged

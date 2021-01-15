3115908cdceaacc89e6dfa05f0185e55d2b8dc14
AzusaChino <azusa146@gmail.com>
2021-01-15 19:28:39 +0800

    2021-01-15 | Get Maximum in Generated Array | 1646

---
+++ cpp/src/2021/jan/getMaximumGeneratedArray.cpp
@@ -0,0 +1,16 @@
+
+f[101] = { 1, 0, 1};
+
+class getMaximumGeneratedArray {
+public:
+    int getMax(int n) {
+        if (f[2] == 0) {
+            for (int i = 2; i <= 100; ++i) {
+                f[i] = i % 2 ? f[i / 2] + f[i / 2 + 1]
+                for (int i = 2; i <= 100; ++i)
+                    f[i] = max(f[i], f[i - 1]);
+            }
+        }
+        return f[n]
+    }
+};

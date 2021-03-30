d6c3e044c7c27f841571b55a90b41992c5f17d55
ycpang <ycpang@iflytek.com>
2021-03-30 16:52:16 +0800

    2021-03-30 | Russian Doll Envelopes | 354

---
+++ cpp/src/2021/mar/russian-doll-envelopes.cc
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxEnvelopes(vector<pair<int, int>> &envelopes)
+    {
+        int size = envelopes.size();
+        sort(envelopes.begin(), envelopes.end(), [](pair<int, int> a, pair<int, int> b) {
+            return a.first < b.first || (a.first == b.first && a.second > b.second);
+        });
+        vector<int> collector;
+        for (auto &pair : envelopes)
+        {
+            auto iter = lower_bound(collector.begin(), collector.end(), pair.second);
+            if (iter == collector.end())
+                collector.push_back(pair.second);
+            else if (*iter > pair.second)
+                *iter = pair.second;
+        }
+        return collector.size();
+    }
+};

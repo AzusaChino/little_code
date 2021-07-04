b33383d83d846864d2e1e177629d6b6074cb6770
AzusaChino <azusa146@gmail.com>
2021-07-04 20:02:07 +0800

    1220. Count Vowels Permutation

---
+++ java/src/main/java/cn/az/code/year2021/july/CountVowelsPermutation.java
@@ -0,0 +1,22 @@
+package cn.az.code.year2021.july;
+
+/**
+ * @author az
+ * @since 2021-07-04
+ */
+public class CountVowelsPermutation {
+
+  public int countVowelPermutation(int n) {
+    int[][] moves = { { 1 }, { 0, 2 }, { 0, 1, 3, 4 }, { 2, 4 }, { 0 } };
+    int[] v = { 1, 1, 1, 1, 1 };
+    while (--n > 0) {
+      int[] v1 = { 0, 0, 0, 0, 0 };
+      for (int i = 0; i < 5; ++i) {
+        for (int j : moves[i])
+          v1[j] = (v1[j] + v[i]) % 1000000007;
+      }
+      v = v1;
+    }
+    return (int) (((long) v[0] + v[1] + v[2] + v[3] + v[4]) % 1000000007);
+  }
+}

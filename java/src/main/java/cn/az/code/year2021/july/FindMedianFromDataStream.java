09fe4da559f6c6206b981887a4a9b003a254edc0
AzusaChino <azusa146@gmail.com>
2021-07-11 19:05:04 +0800

    295. Find Median from Data Stream

---
+++ java/src/main/java/cn/az/code/year2021/july/FindMedianFromDataStream.java
@@ -0,0 +1,24 @@
+package cn.az.code.year2021.july;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author az
+ * @since 2021-07-11
+ */
+public class FindMedianFromDataStream {
+
+    private Queue<Long> small = new PriorityQueue(), large = new PriorityQueue();
+
+    public void addNum(int num) {
+        large.add((long) num);
+        small.add(-large.poll());
+        if (large.size() < small.size()) large.add(-small.poll());
+    }
+
+    public double findMedian() {
+        return large.size() > small.size()
+            ? large.peek()
+            : (large.peek() - small.peek()) / 2.0;
+    }
+}

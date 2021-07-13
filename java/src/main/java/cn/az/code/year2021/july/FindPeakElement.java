b85642d4cda612ab3c04dc01b9a09dc16dbe020b
AzusaChino <azusa146@gmail.com>
2021-07-13 17:22:23 +0800

    162. Find Peak Element

---
+++ java/src/main/java/cn/az/code/year2021/july/FindPeakElement.java
@@ -0,0 +1,27 @@
+package cn.az.code.year2021.july;
+
+/**
+ * @author az
+ * @since 2021-07-13
+ */
+public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+        int N = nums.length;
+        if (N == 1) {
+            return 0;
+        }
+
+        int left = 0, right = N - 1;
+        while (right - left > 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return (left == N - 1 || nums[left] > nums[left + 1]) ? left : right;
+    }
+}

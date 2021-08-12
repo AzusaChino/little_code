ae1841cbca2b3c88b72dd687f3139ca6292172bc
AzusaChino <azusa146@gmail.com>
2021-08-12 19:04:17 +0800

    49. Group Anagrams

---
+++ java/src/main/java/cn/az/code/year2021/aug/GroupAnagrams.java
@@ -0,0 +1,23 @@
+package cn.az.code.year2021.aug;
+
+import java.util.*;
+
+/**
+ * @author az
+ * @since 2021-08-12
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = new char[26];
+            for (char c : s.toCharArray()) ca[c - 'a']++;
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}

1a209cc43d34480d3ba888837589c17ada4e4533
AzusaChino <azusa146@gmail.com>
2021-12-06 09:29:31 +0800

    1217. Minimum Cost to Move Chips to The Same Position

---
+++ cpp/2021/dec/minimum-cost-to-move-chips-to-the-same-position.cc
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minCostToMoveChips(vector<int> &position)
+    {
+        int cnt[2] = {};
+
+        for (auto p : position)
+        {
+            ++cnt[p % 2];
+        }
+        return min(cnt[0], cnt[1]);
+    }
+};

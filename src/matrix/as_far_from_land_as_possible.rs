feaa22d16a89f4e777010f62adbbcfdd9ff0e06b
azusachino <azusa146@gmail.com>
2023-02-10 09:59:53 +0800

    another solution (incorrect)

--- src/matrix/as_far_from_land_as_possible.rs
+++ src/matrix/as_far_from_land_as_possible.rs
@@ -5,6 +5,45 @@ use std::collections::VecDeque;
 const LAND: i32 = 1;
 
 impl Solution {
+    // wrong answer
+    pub fn _max_distance(grid: Vec<Vec<i32>>) -> i32 {
+        let len: usize = grid.len();
+        let mut grid = grid.clone();
+        let mut q = VecDeque::new();
+        for i in 0..len {
+            for j in 0..len {
+                if grid[i][j] == LAND {
+                    q.push_back((i, j, 0));
+                }
+            }
+        }
+
+        if q.is_empty() || q.len() == len * len {
+            return -1;
+        }
+
+        let mut d = -1;
+        let dir: Vec<(i32, i32)> = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+        while let Some((i, j, n)) = q.pop_front() {
+            for (x, y) in &dir {
+                let nx: i32 = i as i32 + *x;
+                let ny: i32 = j as i32 + *y;
+                if nx >= 0
+                    && nx < (n as i32)
+                    && ny >= 0
+                    && ny < (n as i32)
+                    && grid[nx as usize][ny as usize] == 0
+                {
+                    grid[nx as usize][ny as usize] = 1;
+                    q.push_back((nx as usize, ny as usize, n + 1));
+                    d = d.max(n + 1);
+                }
+            }
+        }
+        d
+    }
+
     pub fn max_distance(grid: Vec<Vec<i32>>) -> i32 {
         let mut queue = VecDeque::new();
         let mut dist = vec![vec![i32::MAX; grid[0].len()]; grid.len()];
@@ -59,6 +98,7 @@ mod tests {
 
     #[test]
     fn test() {
-        println!("{}", Solution::default());
+        let grid = vec![vec![1, 0, 1], vec![0, 0, 0], vec![1, 0, 1]];
+        println!("{}", Solution::_max_distance(grid));
     }
 }

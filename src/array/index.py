d35d7d1d8c29483e8daeb24b2902b48dcdf5b272
azusachino <azusachino@proton.me>
2023-11-22 11:02:13 +0900

    [array]diagonal order iteration

--- src/array/index.py
+++ src/array/index.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
@@ -46,6 +47,13 @@ class Solution:
             else:
                 r = m
         return l
+
+    def findDiagonalOrder(self, A: List[List[int]]) -> List[int]:
+        d = defaultdict(list)
+        for i in range(len(A)):
+            for j in range(len(A[i])):
+                d[i+j].append(A[i][j])
+        return [v for k in d.keys() for v in reversed(d[k])]
 
 
 def preSum(nums):

3d73855263cc470a8af62b5f10698863683268e3
azusachino <azusachino@proton.me>
2024-01-25 19:14:43 +0900

    edit distance

---
+++ src/dp/solution.cc
@@ -0,0 +1,61 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int m = text1.size(), n = text2.size();
+        int dp[m + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    int minDistance(string word1, string word2)
+    {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        // base case
+        for (int i = 1; i <= m; i++)
+            dp[i][0] = i;
+        for (int j = 1; j <= n; j++)
+            dp[0][j] = j;
+        // 自底向上求解
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+            }
+        }
+        // 储存着整个 s1 和 s2 的最小编辑距离
+        return dp[m][n];
+    }
+
+    int min(int a, int b, int c)
+    {
+        return std::min(a, std::min(b, c));
+    }
+};
--- src/tree/solution.cc
+++ src/tree/solution.cc
@@ -40,4 +40,18 @@ public:
                 return node->val;
         }
     }
+
+    int pseudoPalindromicPaths(TreeNode *root, int count = 0)
+    {
+        if (!root)
+            return 0;
+
+        // track 1~9 count
+        count ^= 1 << (root->val - 1);
+        int res = this->pseudoPalindromicPaths(root->left, count) + this->pseudoPalindromicPaths(root->right, count);
+        // at leaf, and only contains one odd (a &(a-1)) to check if a is pow of 2 (which is only one significant 1 in binary)
+        if (root->left == root->right && (count & (count - 1)) == 0)
+            res++;
+        return res;
+    }
 };
